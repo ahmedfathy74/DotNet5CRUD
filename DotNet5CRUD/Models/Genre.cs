a07a22c842ef0b9ea7dae7d63745c1f569bbf4dd
ahmed2016170053 <ahmed2016170053@cis.asu.edu.eg>
2023-08-07 16:56:07 +0300

    Add project files.

---
+++ DotNet5CRUD/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5CRUD.Models
+{
+    public class Genre
+    {
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // this line in case type is byte otherwise will create idenetity by default
+        public byte Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
