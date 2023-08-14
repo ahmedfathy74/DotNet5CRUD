a7b4c6c5a8ff57dd48de7e1e31110308d22f23f7
ahmed2016170053 <ahmed2016170053@cis.asu.edu.eg>
2023-08-14 09:32:37 +0300

    apply some modification on CRUD opertion in genere

--- DotNet5CRUD/wwwroot/js/site.js
+++ DotNet5CRUD/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+$('body').delegate('.js-delete', 'click', function(){
+                var btn = $(this);
+    //console.log(btn.data('id'));
+    //var result = confirm('Are you sure that you need to delete this movie?');
 
-// Write your JavaScript code.
+    bootbox.confirm({
+        message: 'Are you sure that you need to delete this movie?',
+    buttons: {
+        confirm: {
+        label: 'Yes',
+    className: 'btn-danger'
+                        },
+    cancel: {
+        label: 'No',
+    className: 'btn-outline-secondary'
+                        }
+                    },
+    callback: function (result) {
+                        if (result) {
+        $.ajax({
+            url: '/movies/delete/' + btn.data('id'),
+            success: function () {
+                //btn.parents('.col-12').fadeOut();
+                var movieContainer = btn.parents('.col-12');
+                movieContainer.addClass('animate__animated animate__zoomOut');
+
+                setTimeout(function () {
+                    movieContainer.remove();
+                }, 1000);
+
+                toastr.success('Movies deleted');
+            },
+            error: function () {
+                toastr.error('Something went Wrong!');
+            }
+        });
+                        }
+                    }
+                });
+    });
